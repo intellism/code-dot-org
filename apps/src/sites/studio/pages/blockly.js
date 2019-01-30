cf110bc04f900a686f55980da6e3944d14e82380
Anjali <anjali@code.org>
2019-01-29 16:24:56 -0800

    [WIP] Add dependency on google/blockly

--- apps/src/sites/studio/pages/blockly.js
+++ apps/src/sites/studio/pages/blockly.js
@@ -1,2 +1,8 @@
 import Blockly from '@code-dot-org/blockly';
 window.Blockly = Blockly;
+
+require('blockly/blockly_compressed.js');
+require('blockly/blocks_compressed.js');
+
+require('blockly/msg/js/en.js');
+require('blockly/javascript_compressed.js');
